0cfe5fe16be687fd892000fdd863f877346cd638
Rashidov01 <avazbekrashidov6@gmail.com>
2024-12-16 19:00:13 +0400

    email address added to the user post inner page changed

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 async function getPost(id: string) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  const res = await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`)
   if (!res.ok) {
     throw new Error('Failed to fetch post')
   }
@@ -15,7 +15,8 @@ export default async function Post({params}: { params: { id: string } }) {
     <div className='container mx-auto px-4 py-8'>
       <div className='max-w-2xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden'>
         <div className='p-8'>
-          <h1 className='text-3xl font-bold mb-4 text-gray-800'>{post.title}</h1>
+          <h1 className='text-3xl font-bold mb-4 text-gray-800'>{post.name}</h1>
+          <span className='font-bold mb-3 text-gray-800'>{post.email}</span>
           <p className='text-gray-600 mb-6 leading-relaxed'>{post.body}</p>
           <Link href='/posts'
             className='inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors duration-300'> Back to all posts </Link>
